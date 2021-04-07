4d986593e132382489d5d9e9f0f3f218ef8f36e2
sandelit <thomas.sandelin@gmail.com>
2021-04-08 00:22:18 +0300

    app.js and started nav.js

--- site/trivia-king/src/App.js
+++ site/trivia-king/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { Broswer as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Route path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Register path="/register" component={Register} />
+            </div>
+        </Router>
+    )
 }
 
 export default App
---
+++ site/trivia-king/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <h2>Trivia King</h2>
+            <Logo />
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Leaderboards</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
